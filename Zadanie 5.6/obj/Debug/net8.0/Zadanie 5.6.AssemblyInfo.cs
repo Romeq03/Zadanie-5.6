568e50b6c23fb39ada4baaac7c5a2fff0f3596bb
Romeq03 <romekbartek18@gmail.com>
2024-02-11 08:19:00 +0100

    nn

--- Zadanie 5.6/obj/Debug/net8.0/Zadanie 5.6.AssemblyInfo.cs
+++ Zadanie 5.6/obj/Debug/net8.0/Zadanie 5.6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zadanie 5.6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30f9d14c7fd495bf346a26ac8ee1449036e8bc87")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zadanie 5.6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zadanie 5.6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
